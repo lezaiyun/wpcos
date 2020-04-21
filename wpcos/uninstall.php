33b807b3cd5ccd8bf69e8464a14703d319425841
laobuluo <37166585+laobuluo@users.noreply.github.com>
2020-04-21 15:46:52 +0800

    move

--- wpcos/uninstall.php
+++
@@ -1,9 +0,0 @@
-<?php
-if(!defined('WP_UNINSTALL_PLUGIN')){
-	exit();
-}
-if (get_option('xos_options')) {
-	delete_option( 'xos_options' );
-}
-delete_option('wpcos_options');
-update_option('upload_url_path', '');
